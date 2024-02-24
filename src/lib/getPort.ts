b7246106bf8b923ed1b480193a71b7fc4c39c11b
Daniel Lopez <dlopez807@gmail.com>
2024-02-23 18:34:20 -0800

    allow for numbers

--- src/lib/getPort.ts
+++ src/lib/getPort.ts
@@ -1,23 +1,27 @@
+const numberToLetter = {
+  2: ['a', 'b', 'c'],
+  3: ['d', 'e', 'f'],
+  4: ['g', 'h', 'i'],
+  5: ['j', 'k', 'l'],
+  6: ['m', 'n', 'o'],
+  7: ['p', 'q', 'r', 's'],
+  8: ['t', 'u', 'v'],
+  9: ['w', 'x', 'y', 'z'],
+}
+
 const getPort = (text: string) => {
-  const numberToLetter = {
-    2: ['a', 'b', 'c'],
-    3: ['d', 'e', 'f'],
-    4: ['g', 'h', 'i'],
-    5: ['j', 'k', 'l'],
-    6: ['m', 'n', 'o'],
-    7: ['p', 'q', 'r', 's'],
-    8: ['t', 'u', 'v'],
-    9: ['w', 'x', 'y', 'z'],
-  }
   const port = [...text.toLowerCase()]
     .map(char => {
+      if (Number.isInteger(Number(char))) {
+        return char
+      }
       const entry = Object.entries(numberToLetter).find(([, value]) => {
         if (value.some(v => v === char)) {
           return true
         } else return false
       })
       const [num] = entry ?? []
-      return num ?? ' '
+      return num ?? ''
     })
     .join('')
   return port
